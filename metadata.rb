51d9c20c8f2d117e3bb921e1ec9b4123b6e56c08
Raj Kiran Gade <raj.gade@relevancelab.com>
2017-01-23 11:06:15 +0530

    version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rajkiran.gade@gmail.com'
 license 'all_rights'
 description 'Installs/Configures awesome_customers_ubuntu'
 long_description 'Installs/Configures awesome_customers_ubuntu'
-version '0.1.0'
+version '1.0.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
